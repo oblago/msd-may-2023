38e8f8a8a38989e4111009b46fe9648f250c3d93
Olga Blagoveshenskaya <olga_blagoveshenskaya@progressive.com>
2023-05-22 12:10:49 -0400

    initial commit on day 1

---
+++ src/JobSolution/JobsApi/Controllers/JobsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JobsApi.Controllers;
+
+[Route("jobs")]
+[ApiController]
+public class JobsController : ControllerBase
+{
+
+    [HttpPost]
+    public async Task<ActionResult> CreateJob([FromBody] JobCreateItem request)
+    {
+        return StatusCode(201, request);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult> GetAllJobs()
+    {
+        var data = new List<JobItemModel>()
+        {
+            new JobItemModel { Id="developer-1", Title="Software Developer 1", Description = "Entry Level Software Developer"},
+            new JobItemModel { Id = "qa-1", Title="Software Quality Assurance 1", Description ="Entry level QA"}
+        };
+        var response = new CollectionResponse<JobItemModel>()
+        {
+            Data = data
+        };
+        return Ok(response);
+    }
+}
